25ef0879de336f86a1c7a31017995229fa4dbddc
BNTCHRT <105383725+BNTNOECHRT@users.noreply.github.com>
2022-10-30 15:42:07 +0100

    final version

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template, jsonify
 
-import pandas as pd
-import numpy as np
-from sklearn.datasets import fetch_california_housing
+from pandas import DataFrame 
+from numpy import array
+from xgboost import Booster, DMatrix
 
 
 import xgboost as xgb
 
 
 app = Flask(__name__)
-model_xgb = xgb.Booster()
+model_xgb = Booster()
 model_xgb.load_model(fname = "my_model.json")
-cols = list(fetch_california_housing(as_frame = True).data.columns)
+cols = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup',
+       'Latitude', 'Longitude']
 
 @app.route('/')
 def home():
@@ -20,16 +21,16 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
-    final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = model_xgb.predict(xgb.DMatrix(data_unseen))
+    final = array(int_features)
+    data_unseen = DataFrame([final], columns = cols)
+    prediction = model_xgb.predict(DMatrix(data_unseen))
     prediction_res = round(prediction[0] * 100000, 2)
     return render_template('home.html',pred='Expected Price will be {:,} $'.format(prediction_res).replace(",", " "))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
-    data_unseen = pd.DataFrame([data])
+    data_unseen = DataFrame([data])
     prediction = model_xgb.predict(data=data_unseen)
     output = prediction[0]
     return jsonify(output)
